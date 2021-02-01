8350a5ddcc21ff643737d1591248e38ae524babd
Stefan Markov <stef4otm@gmail.com>
2021-02-01 19:43:04 +0200

    Add Data Controller

--- Calendar/Calendar.Web/Controllers/DataController.cs
+++ Calendar/Calendar.Web/Controllers/DataController.cs
@@ -1,16 +1,35 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Calendar.App.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace Calendar.Web.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class DataController : ControllerBase
+    public class DataController : Controller
     {
-        
+        private readonly IDataService dataService;
+
+        public DataController(IDataService dataService)
+        {
+            this.dataService = dataService;
+        }
+
+        [HttpPost]
+        public IActionResult ReserveDate(DateTime date)
+        {
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            dataService.ReserveDate(date, userId);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult AddAvailableDate(DateTime date, decimal price)
+        {
+            dataService.AddAvailableDate(date, price);
+
+            return Ok();
+        }
     }
 }
